5bc59041c502d873d4e2d40e9c1e5f493da5dd1d
Arthur Neves <arthurnn@gmail.com>
2013-08-14 21:36:31 -0400

    Array is a Enumerable

--- lib/liquid/standardfilters.rb
+++ lib/liquid/standardfilters.rb
@@ -274,7 +274,7 @@ module Liquid
 
       def initialize(input)
         @input =
-          if input.is_a?(Array) || input.kind_of?(Enumerable)
+          if input.kind_of?(Enumerable)
             input
           else
             Array(input)
